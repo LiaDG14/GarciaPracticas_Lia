5f28ee8cf56c4b4f3f7f48de73e67e8f47d99a2c
LiaDG14 <garcialiadamaris@gmail.com>
2023-11-19 19:06:25 +0100

    Added MultiplyTableFull

--- src/main/java/ud2/practicas/MultiplyTableFull.java
+++ src/main/java/ud2/practicas/MultiplyTableFull.java
@@ -7,6 +7,8 @@ public class MultiplyTableFull {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
 
+
+
         for (int i = 1; i <= 10; i++) {
             for (int j = 5; j <= 20; j++) {
                 System.out.printf(" %3d", i*j);
